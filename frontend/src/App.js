38123cf5c9767e31fa8cef02f930ac3845c83702
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-01 18:07:10 +0800

    App have been modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,12 @@ const App = () =>{
   return (
     <>
     <Header />
-    <main>
-      <Container>
-        <h1>Welcome to ProShop</h1>
-      </Container>
-    
-    </main>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      
+      </main>
     <Footer />
     </>
   );
