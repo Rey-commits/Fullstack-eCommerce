b3725f63e5ced47b0421e07fea61e8d9c59049af
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-01 18:07:45 +0800

    Header have been modified

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,16 +4,18 @@ import { Navbar, Nav, Container } from "react-bootstrap"
 const Header = () => {
     return (
         <header>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                <Navbar.Brand href="/">ProShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/cart">Cart</Nav.Link>
-                        <Nav.Link href="/login">Sign In</Nav.Link>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                        <Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
                     </Nav>
                     
                 </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
