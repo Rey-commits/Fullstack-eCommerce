ff3558bf73da2e031f8fabb3a81b19f537948fdb
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-01 17:45:24 +0800

    Navbar have been added to Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Navbar, Nav, Container } from "react-bootstrap"
 
 const Header = () => {
     return (
         <header>
-           header 
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/login">Sign In</Nav.Link>
+                    </Nav>
+                    
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 }
