e2c971f80ec0903355d71fab33b5547c3c74292f
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-03 00:55:29 +0800

    products have been modified

--- backend/data/products.js
+++ backend/data/products.js
@@ -77,6 +77,33 @@ const products = [
     rating: 4,
     numReviews: 12,
   },
+  {
+    _id: '5',
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '3',
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  
 ]
 
 export default products
