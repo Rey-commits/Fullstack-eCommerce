8f497b66d52ded7fa8feeb3853312af8ab09986a
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-05-04 08:29:45 +0800

    typo has been corrected

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -37,7 +37,7 @@ const ProductScrren = ({match}) => {
                             Price: ${product.price}
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            Description: ${product.description}
+                            Description: {product.description}.
                         </ListGroup.Item>
                     </ListGroup>
                 </Col>
